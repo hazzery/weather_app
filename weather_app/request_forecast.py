d32e7c65862a8996861ce51332aaa46f860ec0a5
Harry Parkes <harry.parkes@beca.com>
2024-02-09 14:48:56 +1300

    renamed API_TOKEN to WEATHER_TOKEN

--- weather_app/request_forecast.py
+++ weather_app/request_forecast.py
@@ -7,10 +7,10 @@ from weather_app.classes import ApiResponse
 
 def request_forecast(latitude: float, longitude: float) -> ApiResponse:
     try:
-        with open("API_TOKEN", "r") as file:
+        with open("WEATHER_TOKEN", "r") as file:
             key = file.read()
     except FileNotFoundError:
-        sys.exit("API_TOKEN file not found")
+        sys.exit("WEATHER_TOKEN file not found")
 
     response = requests.get(
         "https://api.openweathermap.org/data/2.5/forecast"
