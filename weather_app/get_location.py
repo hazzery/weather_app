7cfe4ae740271b8cec5c20024f2fc6b95a0bbaa6
Harry Parkes <harry.parkes@beca.com>
2024-02-09 15:17:32 +1300

    added get_location to get lat, lon of place name

---
+++ weather_app/get_location.py
@@ -0,0 +1,21 @@
+import sys
+
+from opencage.geocoder import OpenCageGeocode
+
+
+def get_location(location_name: str) -> tuple[float, float]:
+    """Get the latitude and longitude of a location.
+
+    :param location_name: The name of the location.
+    :return: The latitude and longitude of the location.
+    """
+    try:
+        with open("../MAP_TOKEN", "r") as file:
+            key = file.read()
+    except FileNotFoundError:
+        sys.exit("MAP_TOKEN file not found")
+
+    geocoder = OpenCageGeocode(key)
+    results = geocoder.geocode(location_name)
+
+    return results[0]["geometry"]["lat"], results[0]["geometry"]["lng"]
