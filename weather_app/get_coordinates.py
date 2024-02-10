604f2f678f05e701a6bdefe9e5c005a232535802
Harry Parkes <harrydparkes@proton.me>
2024-02-10 16:10:42 +1300

    changed get_location to get_coordinates

--- weather_app/get_coordinates.py
+++ weather_app/get_coordinates.py
@@ -3,14 +3,14 @@ import sys
 from opencage.geocoder import OpenCageGeocode
 
 
-def get_location(location_name: str) -> tuple[float, float]:
+def get_coordinates(location_name: str) -> tuple[float, float]:
     """Get the latitude and longitude of a location.
 
     :param location_name: The name of the location.
     :return: The latitude and longitude of the location.
     """
     try:
-        with open("../MAP_TOKEN", "r") as file:
+        with open("MAP_TOKEN", "r") as file:
             key = file.read()
     except FileNotFoundError:
         sys.exit("MAP_TOKEN file not found")
