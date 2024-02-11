8b5b0e4ea789357956d1bae9286554345799975f
Harry Parkes <harrydparkes@proton.me>
2024-02-12 08:16:10 +1300

    Changed weather_widget to fit new data structure

--- weather_app/weather_widget.py
+++ weather_app/weather_widget.py
@@ -1,29 +1,30 @@
-import dataclasses
 import datetime
 import tkinter
 
-from classes import WeatherData, Main
+from .weather import WeatherForecast
+from .classes import City
 
 
 class DailyWeather(tkinter.Frame):
-    weather_columns = dataclasses.fields(Main)
+    weather_columns = ("Time", "Temperature", "Feels Like", "Min Temp", "Max Temp")
 
     def __init__(
             self,
             master: tkinter.Misc,
-            date: datetime.date,
-            weather_data: list[WeatherData],
-            **kwargs
+            weather_data: list[WeatherForecast]
     ) -> None:
-        super().__init__(master, kwargs)
+        """Displays the weather data for a single day.
 
-        tkinter.Label(self, text="Time").grid(row=0, column=0)
-        for column, title in enumerate(self.weather_columns, start=1):
-            tkinter.Label(self, text=title.name).grid(row=0, column=column)
+        :param master: The parent widget.
+        :param weather_data: The weather data for the day.
+        """
+        super().__init__(master)
+
+        for column, title in enumerate(self.weather_columns):
+            tkinter.Label(self, text=title).grid(row=0, column=column)
 
         for row, weather in enumerate(weather_data, start=1):
-            tkinter.Label(self, text=date.strftime("%A %d %B")).grid(row=row, column=0)
-            for column, value in enumerate(dataclasses.asdict(weather.main).values(), start=1):
+            for column, value in enumerate(weather):
                 tkinter.Label(self, text=value).grid(row=row, column=column)
 
 
@@ -32,10 +33,17 @@ class WeatherTable(tkinter.Frame):
     def __init__(
             self,
             master: tkinter.Misc,
-            forecasts: dict[datetime.date, list[WeatherData]],
-            **kwargs
+            city: City,
+            forecasts: dict[datetime.date, list[WeatherForecast]],
     ) -> None:
-        super().__init__(master, kwargs)
+        """Displays the weather forecasts for a city.
+
+        :param master: The parent widget.
+        :param city: The city.
+        :param forecasts: The weather forecasts.
+        """
+        super().__init__(master)
+        tkinter.Label(self, text=city.name + " Weather").pack()
         for date, weather_data in forecasts.items():
-            DailyWeather(self, date, weather_data).pack()
-            tkinter.Label(self, text="").pack()
+            tkinter.Label(self, text=str(date)).pack()
+            DailyWeather(self, weather_data).pack()
