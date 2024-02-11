521a3e5cb57fbee965e1f7e52425762dc1c78a6b
Harry Parkes <harrydparkes@proton.me>
2024-02-12 08:32:00 +1300

    using matplotlib to draw errorbar graphs to show weather over time

--- weather_app/weather_widget.py
+++ weather_app/weather_widget.py
@@ -1,31 +1,53 @@
 import datetime
 import tkinter
 
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.figure import Figure
+
 from .weather import WeatherForecast
 from .classes import City
 
 
-class DailyWeather(tkinter.Frame):
-    weather_columns = ("Time", "Temperature", "Feels Like", "Min Temp", "Max Temp")
-
+class WeatherPlot(tkinter.Frame):
     def __init__(
-            self,
-            master: tkinter.Misc,
-            weather_data: list[WeatherForecast]
+            self, parent: tkinter.Misc, weather_data: list[WeatherForecast]
     ) -> None:
-        """Displays the weather data for a single day.
+        super().__init__(parent)
+        figure = Figure(figsize=(5, 4), dpi=100)
+        axes = figure.add_subplot(1, 1, 1)
+        axes.errorbar(
+            [forecast[0].hour for forecast in weather_data],
+            [forecast[1] for forecast in weather_data],
+            (
+                [forecast[3] for forecast in weather_data],
+                [forecast[4] for forecast in weather_data]
+            ))
+        canvas = FigureCanvasTkAgg(figure, master=self)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=True)
 
-        :param master: The parent widget.
-        :param weather_data: The weather data for the day.
-        """
-        super().__init__(master)
 
-        for column, title in enumerate(self.weather_columns):
-            tkinter.Label(self, text=title).grid(row=0, column=column)
-
-        for row, weather in enumerate(weather_data, start=1):
-            for column, value in enumerate(weather):
-                tkinter.Label(self, text=value).grid(row=row, column=column)
+# class DailyWeather(tkinter.Frame):
+#     weather_columns = ("Time", "Temperature", "Feels Like", "Min Temp", "Max Temp")
+#
+#     def __init__(
+#             self,
+#             master: tkinter.Misc,
+#             weather_data: list[WeatherForecast]
+#     ) -> None:
+#         """Displays the weather data for a single day.
+#
+#         :param master: The parent widget.
+#         :param weather_data: The weather data for the day.
+#         """
+#         super().__init__(master)
+#
+#         for column, title in enumerate(self.weather_columns):
+#             tkinter.Label(self, text=title).grid(row=0, column=column)
+#
+#         for row, weather in enumerate(weather_data, start=1):
+#             for column, value in enumerate(weather):
+#                 tkinter.Label(self, text=value).grid(row=row, column=column)
 
 
 class WeatherTable(tkinter.Frame):
@@ -46,4 +68,4 @@ class WeatherTable(tkinter.Frame):
         tkinter.Label(self, text=city.name + " Weather").pack()
         for date, weather_data in forecasts.items():
             tkinter.Label(self, text=str(date)).pack()
-            DailyWeather(self, weather_data).pack()
+            WeatherPlot(self, weather_data).pack()
