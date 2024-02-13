cc7846058a72843d6a772f9ab088a05159353b14
Harry Parkes <harrydparkes@proton.me>
2024-02-13 19:23:04 +1300

    weather icon image requests

---
+++ weather_app/request_image.py
@@ -0,0 +1,13 @@
+from PIL import ImageTk
+import requests
+
+
+def request_image(icon_id: str) -> ImageTk.PhotoImage:
+    """Request the image for the weather icon.
+
+    :param icon_id: The OpenWeatherMap icon id.
+    :return: The image.
+    """
+    print(icon_id)
+    response = requests.get(f"https://openweathermap.org/img/wn/{icon_id}.png")
+    return ImageTk.PhotoImage(data=response.content)
